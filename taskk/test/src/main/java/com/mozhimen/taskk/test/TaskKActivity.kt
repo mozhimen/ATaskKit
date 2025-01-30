4a5f550b696fb196cd9d0adc7cb3bae755df0a06
Kolin Zhao & Mozhimen <mozhimen@163.com>
2025-01-30 21:24:41 +0800

    250130

---
+++ taskk/test/src/main/java/com/mozhimen/taskk/test/TaskKActivity.kt
@@ -0,0 +1,24 @@
+package com.mozhimen.taskk.test
+
+import android.view.View
+import com.mozhimen.bindk.bases.viewdatabinding.activity.BaseActivityVDB
+import com.mozhimen.kotlin.utilk.android.content.startContext
+import com.mozhimen.taskk.test.databinding.ActivityTaskkBinding
+
+class TaskKActivity : BaseActivityVDB<ActivityTaskkBinding>() {
+    fun goTaskKAsync(view: View) {
+        startContext<TaskKAsyncActivity>()
+    }
+
+    fun goTaskKCountDown(view: View) {
+        startContext<TaskKCountDownActivity>()
+    }
+
+    fun goTaskKLocation(view: View) {
+        startContext<TaskKLocationActivity>()
+    }
+
+    fun goTaskKPoll(view: View) {
+        startContext<TaskKPollActivity>()
+    }
+}
---
+++ taskk/test/src/main/java/com/mozhimen/taskk/test/TaskKLocationActivity.kt
@@ -0,0 +1,31 @@
+package com.mozhimen.taskk.test
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import com.mozhimen.bindk.bases.viewdatabinding.activity.BaseActivityVDB
+import com.mozhimen.kotlin.lintk.optins.OApiCall_BindLifecycle
+import com.mozhimen.kotlin.lintk.optins.OApiInit_ByLazy
+import com.mozhimen.kotlin.lintk.optins.permission.OPermission_ACCESS_COARSE_LOCATION
+import com.mozhimen.kotlin.lintk.optins.permission.OPermission_ACCESS_FINE_LOCATION
+import com.mozhimen.kotlin.elemk.android.cons.CPermission
+import com.mozhimen.taskk.temps.TaskKLocation
+import com.mozhimen.kotlin.utilk.kotlin.UtilKLazyJVM.lazy_ofNone
+import com.mozhimen.kotlin.utilk.wrapper.UtilKPermission
+import com.mozhimen.permissionk.PermissionK
+import com.mozhimen.taskk.test.databinding.ActivityTaskkLocationBinding
+
+class TaskKLocationActivity : BaseActivityVDB<ActivityTaskkLocationBinding>() {
+    @OptIn(OApiInit_ByLazy::class, OApiCall_BindLifecycle::class)
+    private val _taskKLocation by lazy_ofNone { TaskKLocation().apply { bindLifecycle(this@TaskKLocationActivity) } }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    @OptIn(OApiInit_ByLazy::class, OApiCall_BindLifecycle::class, OPermission_ACCESS_FINE_LOCATION::class, OPermission_ACCESS_COARSE_LOCATION::class)
+    override fun initView(savedInstanceState: Bundle?) {
+        if (UtilKPermission.isSelfGranted(arrayOf(CPermission.ACCESS_COARSE_LOCATION, CPermission.ACCESS_FINE_LOCATION)))
+            _taskKLocation.startLocationTask(1000) { a1, a2 ->
+                vdb.taskkLocationTxt.text = "$a1, $a2"
+            }
+        else
+            PermissionK.requestPermissions(this, arrayOf(CPermission.ACCESS_COARSE_LOCATION, CPermission.ACCESS_FINE_LOCATION))
+    }
+}
